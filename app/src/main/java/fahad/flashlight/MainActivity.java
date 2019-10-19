057e02ffa86caf4a5fa0cc503796fee063d8c5e6
itxfahad123 <its.fahad.000@gmail.com>
2019-10-20 00:55:41 +0500

    UpDated Double Clicking Fault and app will able to work with all the version of android

--- app/src/main/java/fahad/flashlight/MainActivity.java
+++ app/src/main/java/fahad/flashlight/MainActivity.java
@@ -3,18 +3,23 @@ package fahad.flashlight;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
-import android.hardware.camera2.CameraAccessException;
+import android.hardware.Camera;
 import android.hardware.camera2.CameraManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 
+
 public class MainActivity extends AppCompatActivity {
 
     ImageView imageView;
 
-    boolean Switch = false;
+    boolean witch = true;
+
+    Camera mCamera;
+
+    Camera.Parameters parameters;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,12 +27,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         imageView = findViewById(R.id.imageView);
-
-
-
-
-
-
     }
 
     public void press(View view)
@@ -35,31 +34,25 @@ public class MainActivity extends AppCompatActivity {
         if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.M)
         {
             CameraManager cameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
-
             String CameraID= null;
 
             if (cameraManager != null) {
                 try {
-
                     CameraID = cameraManager.getCameraIdList()[0];
-
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
             }
 
 
-            if(Switch == true)
+            if(witch == true)
             {
                 imageView.setImageResource(R.drawable.on);
-
                 Switch = false;
-
                 if (cameraManager != null) {
-                    try {
-
+                    try
+                    {
                         cameraManager.setTorchMode(CameraID,true);
-
                     } catch (Exception e) {
                         e.printStackTrace();
                     }}
@@ -69,27 +62,37 @@ public class MainActivity extends AppCompatActivity {
             else
             {
                 imageView.setImageResource(R.drawable.off);
-
                 Switch = true;
-
                 if (cameraManager != null) {
                     try {
-
                         cameraManager.setTorchMode(CameraID,false);
-
                     } catch (Exception e) {
                         e.printStackTrace();
                     }}
-
-
             }
-
-
         }
-
-
+        if(Build.VERSION.SDK_INT<Build.VERSION_CODES.M)
+        {
+            if(witch == true)
+            {
+                imageView.setImageResource(R.drawable.on);
+                witch = false;
+                mCamera = Camera.open();
+                parameters = mCamera.getParameters();
+                parameters.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
+                mCamera.setParameters(parameters);
+                mCamera.startPreview();
+            }
+        else
+            {
+                imageView.setImageResource(R.drawable.off);
+                witch = true;
+                mCamera = Camera.open();
+                parameters = mCamera.getParameters();
+                parameters.setFlashMode(Camera.Parameters.FLASH_MODE_OFF);
+                mCamera.setParameters(parameters);
+                mCamera.stopPreview();
+            }
+        }
     }
-
-
-
 }
